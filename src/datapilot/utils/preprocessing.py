2b8675c593831cca8c6e29a4eaf146bee75fa454
Danil <ddrxg@MacBook-Pro-Danil.local>
2026-03-19 14:48:27 +0300

    препроцессинг норм работает, можно тестить в проде

--- src/datapilot/utils/preprocessing.py
+++ src/datapilot/utils/preprocessing.py
@@ -2,7 +2,7 @@ from typing import Literal
 from pandas import DataFrame
 
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
 # Это утилита для EDA класса для того, чтобы выявить нормальный способ
 # препроцессинка признаков в фрейме
 
@@ -11,9 +11,7 @@ def preprocessing(df: DataFrame | None = None,
                   model_type: Literal['linear', 'tree'] = 'linear',
                   scaler_columns: list[str] | None = None, 
                   labels_columns: list[str] | None = None, 
-                  type_scaler: Literal['minmax', 'standart'] = 'standart', 
-                  target: str | None = None, 
-                  target_encoding: Literal['target_smoothing', 'target_noise', 'cv_folds'] = 'target_smoothing'):
+                  type_scaler: Literal['minmax', 'standart'] = 'standart'):
     """
     **uv**:
     unique values -> Up to what number of unique values in a column's\n
@@ -28,19 +26,12 @@ def preprocessing(df: DataFrame | None = None,
     отражают их степень веса, то их необходимо указать.
     Например: уровень образования бакалавр -> 1, средне-специальное -> 0
 
+    **scaler_columns**:
+    Какие колонки необходимо заскейлить
+
     **type_scaler**:
     default StandartScaler()\n
     you can change to MinMaxScaler()
-
-    **target**:
-    И дураку понятно, что это целевая метка
-
-    **target_encoding**:
-    Тип кодирования целевой метки
-    
-    **a**:
-    ...
-
     """
     if df is None:
         raise ValueError("df must be provided")
@@ -66,22 +57,15 @@ def preprocessing(df: DataFrame | None = None,
     # --- 2. Labels columns ---
     if labels_columns is not None and isinstance(labels_columns, list):
         for col in labels_columns:
+            encoder = LabelEncoder()
             if col in df.columns:
-                df[col] = df[col].astype("category").cat.codes
+                df[col] = encoder.fit_transform(df[col]) # type: ignore
 
-                if col in categorical_cols:
-                    categorical_cols.remove(col)
-                if col not in numeric_cols:
-                    numeric_cols.append(col)
-
-    # --- 3. Target encoding ---
-    # пока скип    
-
-    # --- 4. One-Hot для линейных моделей ---
+    # --- 3. One-Hot для линейных моделей ---
     if model_type == "linear" and categorical_cols:
         df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
     
-    # --- 5. Scaling ---
+    # --- 4. Scaling ---
     if model_type == "linear" and scaler_columns:
         scaler = StandardScaler() if type_scaler == "standart" else MinMaxScaler()
 
